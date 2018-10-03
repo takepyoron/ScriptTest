cf394667b1f79555dfce871db8bb24ceb0ce02d2
takepyoron <kekero13@softbank.ne.jp>
2018-10-03 19:08:58 +0900

    ScriptTestプロジェクト　初めてのコミット
    Lesson4「Ｃ#スクリプトの基礎」の練習と課題提出のために作ったファイルです。

---
+++ Assets/hattennkadai.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int mp = 53;　　//mp
+
+    //魔法攻撃用の関数
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りＭＰは" + mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない");
+        }
+    }
+}
+
+public class hattennkadai : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss boss = new Boss();
+
+        //魔法攻撃用の関数を10回呼び出す
+        for (int i = 0; i < 10; i++)
+        {
+            boss.Magic();
+        }
+        //魔法攻撃用の関数をもう一度呼び出す
+        boss.Magic();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/kadai.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kadai : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //配列を初期化する
+        int[] array = { 2, 4, 6, 8, 10 };
+        for (int i = 0; i < array.Length; i++ )
+        {
+            Debug.Log(array[i]);
+
+        }
+        for(int k = array.Length - 1; k >= 0; k--)
+        {
+            Debug.Log(array[k]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
